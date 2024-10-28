ca767f9a648596325cf4f93526d643703e27e9ec
23030219URIEL <23030219@itesa.edu.mx>
2024-10-28 09:07:24 -0600

    Version_02: Método Agregar elemento y ToString

---
+++ ListaLigada.UI.Consola/Program.cs
@@ -0,0 +1,24 @@
+﻿using ListaLigada.Logica;
+
+internal class Program
+
+{
+    private static void Main(string[] args)
+    {
+        
+
+
+
+        var singleList = new SingleList<string>();
+
+        singleList.Add("Alondra");
+        singleList.Add("Karla");
+        singleList.Add("Pris");
+        singleList.Add("Luis");
+        singleList.Add("Uriel");
+
+        Console.WriteLine(singleList);
+
+        Console.ReadKey();
+    }
+}
